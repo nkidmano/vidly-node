e598409adb85c8257af09b11f16b98a499ea3d4c
nkidmano <nobudy55@gmail.com>
2018-04-28 10:44:00 +0700

    added lodash bcrypt package, added user model routes and hash password

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
+const users = require('./routes/users');
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
 const app = express();
 
 mongoose.connect('mongodb://localhost/vidly')
@@ -13,6 +17,8 @@ app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
+app.use('/api/users', users);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- routes/genres.js
+++ routes/genres.js
@@ -12,8 +12,8 @@ router.post('/', async (req, res) => {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
-  let genre = new Genre({ name: req.body.name });
-  genre = await genre.save();
+  const genre = new Genre({ name: req.body.name });
+  await genre.save();
 
   res.send(genre);
 });
@@ -23,9 +23,8 @@ router.put('/:id', async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
 
   const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true });
-  
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-  
+
   res.send(genre);
 });
 
